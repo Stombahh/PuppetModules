8cc37d539d557bd97444be44698d11be2483b3ae
Stombahh <tommi.ilvonen@gmail.com>
2017-04-17 16:06:51 +0300

    made fixes

--- apache2/manifests/init.pp
+++ apache2/manifests/init.pp
@@ -16,20 +16,20 @@ class apache2 {
 		require => Package["apache2"],
 	}
 	
-	file {‘/etc/apache2/mods-enabled/userdir.conf’:
+	file {'/etc/apache2/mods-enabled/userdir.conf':
 		ensure => “link”,
 		target => “/etc/apache2/mods-available/userdir.conf”,
 		notify => Service[“apache2”],
 		require => Package[“apache2”],
 	}
 
-	file {‘/home/tommi/public_html’:
+	file {'/home/tommi/public_html':
 		ensure => “directory”,
 		owner => “tommi”,
 		group => “tommi”,
 	}
 
-	file {‘/home/tommi/public_html/index.html’:
+	file {'/home/tommi/public_html/index.html':
 		content => template(“apache2/index”),
 		owner => “tommi”,
 		group => “tommi”,
