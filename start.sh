669e87762893513abd8b3e4ce4f733a0ed781e5f
Tommi Ilvonen <tommi.ilvonen@myy.haaga-helia.fi>
2017-05-16 01:36:05 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install git puppet
-git clone url https://github.com/Stombahh/puppet.git
+git clone https://github.com/Stombahh/puppet.git
 cd puppet
 bash apply.sh
